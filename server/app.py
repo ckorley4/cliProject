198872a8a3d0f058bc8e8c45addd120489532d15
ckorley4 <ckorley4uo@yahoo.com>
2024-02-16 19:58:23 -0500

    New

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from config import app, migrate
 
 from models import db
+def home():
+  print("Welcome to Our Book Store")
 
 if __name__ == "__main__":
   with app.app_context():
     migrate.init_app(app, db)
-    pass
+    home()
     # remove pass and write your cli logic
--- server/models.py
+++ server/models.py
@@ -1,2 +1,22 @@
 from config import db
 
+class Book(db.Model):
+    __tablename__='books'
+
+    isbn=db.Column(db.Integer,primary_key=True)
+    author=db.Column(db.String,unique=True)
+    price=db.Column(db.Integer)
+    sale_date=db.Column(db.DateTime, server_default=db.func.now())
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
+
+class Buyer(db.Model):
+    __tablename__='buyers'
+    
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String)
+    address=db.Column(db.String)
+    book_isbn=db.Column(db.Integer, db.ForeignKey('books.isbn'))
+    card_number=db.Column(db.Integer)
+    purchase_date=db.Column(db.DateTime, server_default=db.func.now())
+    
